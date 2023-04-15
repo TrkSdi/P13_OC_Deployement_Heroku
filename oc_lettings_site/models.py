10f1eba860fae6f5e9db9a7fed53e9ed061b64e6
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-04-15 23:24:53 +0200

    pep8 issue fixed & pluralisation

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -13,6 +13,9 @@ class Address(models.Model):
 
     def __str__(self):
         return f'{self.number} {self.street}'
+    
+    class Meta:
+        verbose_name_plural = "Addresses"
 
 
 class Letting(models.Model):

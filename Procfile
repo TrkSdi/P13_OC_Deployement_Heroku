dd6ba73e15705497b085e60188c409d3fa59cd58
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-05-27 23:14:34 +0200

    deploying test

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn --pythonpath Python-OC-Lettings-FR app:app
+release: python3 manage.py migrate
+web: gunicorn Python-OC-Lettings-FR.wsgi --log-file -
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'fast-gorge-63898.herokuapp.com']
 
 
 # Application definition
@@ -109,3 +109,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

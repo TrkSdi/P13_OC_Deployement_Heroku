2ceaed35cf382fdf4d8beeb442e0c837769ad0d6
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-05-28 09:06:44 +0200

    removing comments from data_transfer script

--- db_data_transfer.py
+++ db_data_transfer.py
@@ -1,68 +1,7 @@
 import sqlite3
 import pandas as pd
 
-'''
-def open_db():
-    con = sqlite3.connect('Python-OC-Lettings-FR/oc-lettings-site.sqlite3')
-    cur = con.cursor()
 
-    # To add > Create folder data
-    path = 'Python-OC-Lettings-FR/data_csv'
-    os.makedirs(path, exist_ok=True)
-
-
-    data1 = cur.execute("SELECT * FROM oc_lettings_site_address")
-    with open('Python-OC-Lettings-FR/data_csv/adresses.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data1)
-
-    data2 = cur.execute("SELECT * FROM oc_lettings_site_profile")
-    with open('Python-OC-Lettings-FR/data_csv/profiles.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data2)
-
-    data3 = cur.execute("SELECT * FROM oc_lettings_site_letting")
-    with open('Python-OC-Lettings-FR/data_csv/lettings.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data3)
-
-    data4 = cur.execute("SELECT * FROM auth_user")
-    with open('Python-OC-Lettings-FR/data_csv/users.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data4)
-
-def copy_csv_to_table():
-
-    data1 = pd.read_csv('Python-OC-Lettings-FR/data_csv/adresses.csv',
-                        names=["id",
-                               "number",
-                               "street",
-                               "city",
-                               "state",
-                               "zip_code",
-                               "country_iso_code"],)
-
-    con = sqlite3.connect('Python-OC-Lettings-FR/oc-lettings-site.sqlite3')
-    data1.to_sql('lettings_address', con, index=False, if_exists='replace')
-
-
-    con.close()
-
-def show_tables():
-    con = sqlite3.connect('Python-OC-Lettings-FR/oc-lettings-site.sqlite3')
-    print("Connexion établie avec la base de données")
-    cur = con.cursor()
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cur.fetchall()
-    print(tables)
-    for table in tables:
-        print(table[0])
-    con.close()
-
-show_tables()
-open_db()
-copy_csv_to_table()
-'''
 path_db = "Python-OC-Lettings-FR/oc-lettings-site.sqlite3"
 path_csv = "Python-OC-Lettings-FR/data_csv/"
 

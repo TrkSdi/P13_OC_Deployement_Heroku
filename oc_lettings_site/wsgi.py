1e2bfac8129f64aaf59bba9bd1d5006b206e8955
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-06-02 15:30:02 +0200

    adding whithe noise

--- oc_lettings_site/wsgi.py
+++ oc_lettings_site/wsgi.py
@@ -1,7 +1,12 @@
 import os
-
+import settings
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
 
-application = get_wsgi_application()
+application_default = get_wsgi_application()
+application = WhiteNoise(application_default, root=settings.STATIC_ROOT)
+#application.add_files("/path/to/more/static/files", prefix="more-files/")
+
+

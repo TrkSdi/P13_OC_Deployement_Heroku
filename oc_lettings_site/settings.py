051664d33184be22c280662c069effa5ce670830
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-06-07 10:31:39 +0200

    fix deployment

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'p13-oc.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'oc-13.herokuapp.com', 'localhost']
 
 
 # Application definition
